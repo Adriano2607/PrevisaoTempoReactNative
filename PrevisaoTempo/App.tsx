499c80fdd55a14bf63560e8dae8f2bbbd81fd11e
Adriano2607 <rodrigues.adriano2607@gmail.com>
2024-02-27 03:18:29 -0300

    funciona

--- PrevisaoTempo/App.tsx
+++ PrevisaoTempo/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TelaHome from './src/TelaHome';
+import TelaHome from './src/screens/TelaHome';
 
 export default function App() {
   return (
